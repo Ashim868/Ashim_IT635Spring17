33e211b948cfaba235f0c0f296b1470f58ae62ff
Ashim868 <as868@njit.edu>
2017-03-21 08:22:22 -0400

    Add files via upload

---
+++ IT635midterm_proj/includes/footer.php
@@ -0,0 +1,14 @@
+</div>
+<!-- /container -->
+
+	<!-- file input -->
+	<script Type='text/javascript' src="assets/plugins/fileinput/js/plugins/canvas-to-blob.min.js"></script>
+	<script Type='text/javascript' src="assets/plugins/fileinput/js/plugins/sortable.min.js"></script>
+	<script Type='text/javascript' src="assets/plugins/fileinput/js/plugins/purify.min.js"></script>
+	<script Type='text/javascript' src="assets/plugins/fileinput/js/fileinput.min.js"></script>
+	<!-- datatables js -->
+	<script Type='text/javascript' src="assets/plugins/datatables.min.js"></script>
+
+</body>
+</html>
+
